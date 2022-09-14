11bfcd3359358af9dec87c43bc95b0be31c4aa15
Eduardo G Brandão <Edu.gbrandao@gmail.com>
2022-09-14 16:11:41 -0300

    Adicionado/Definido novas classes

--- Proj_Game/Entities/Obstacles/obstacle.cpp
+++ Proj_Game/Entities/Obstacles/obstacle.cpp
@@ -1,1 +1,10 @@
 #include "obstacle.h"
+
+Obstacles::Obstacle::Obstacle() :
+	Entity()
+{};
+Obstacles::Obstacle::Obstacle(const unsigned int _type, const sf::RectangleShape& _hitBox, float* elapsed_timeRef, const std::string textureRef, const sf::IntRect sheetCut) :
+	Entity(_type, _hitBox, elapsed_timeRef, textureRef, sheetCut)
+{};
+Obstacles::Obstacle::~Obstacle()
+{};
--- Proj_Game/Entities/Obstacles/obstacle.h
+++ Proj_Game/Entities/Obstacles/obstacle.h
@@ -8,18 +8,11 @@ namespace Obstacles
 	{
 	public:
 		Obstacle();
-		Obstacle(const unsigned int _type, const sf::RectangleShape& _hitBox, const sf::IntRect sheetCut = sf::IntRect(), const std::string textureRef = "");
+		Obstacle(const unsigned int _type, const sf::RectangleShape& _hitBox, float* elapsed_timeRef, const std::string textureRef = "", const sf::IntRect sheetCut = sf::IntRect());
 		~Obstacle();
 
-
-		const sf::Vector2f GetPosition();
-		void MovePosition(const sf::Vector2f _newPosition);
-
-		void Execute();
-		void SelfPrint(sf::RenderWindow& context_window);
-		void Collided(const Entity* _other);
-
-	private:
-
+		virtual void Execute() = 0;
+		virtual void SelfPrint(sf::RenderWindow& context_window) = 0;
+		virtual void Collided(const int type, const sf::Vector2f& movement) = 0;
 	};
 }
